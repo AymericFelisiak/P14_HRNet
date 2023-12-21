a65afb2bb15be022f302ea17227432951ecf4b13
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-12-21 19:34:42 +0100

    Added dropdownmenu react package

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,113 +1,63 @@
 import React from 'react';
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../../store/EmployeeSlice';
+import DropDownMenu  from 'p14_dropdownmenu';
+import states from '../../data/StateList.json';
+import departments from '../../data/DepartmentList.json';
 
 export default function Form() {
-    const [employee, setEmployee] = useState({});
 
     const dispatch = useDispatch();
 
     const handleAddEmployee = (e) => {
         e.preventDefault();
-        dispatch(addEmployee(employee));
+        const form = new FormData(e.target);
+        const data = Object.fromEntries(form.entries());
+        dispatch(addEmployee(data));
     };
 
     return (
         <form action="#" id="create-employee" onSubmit={handleAddEmployee}>
             <label htmlFor="first-name">First Name</label>
-            <input
-                type="text"
-                id="first-name"
-                onChange={(e) =>
-                    setEmployee({ ...employee, firstname: e.target.value })
-                }
-            />
+            <input name="first-name" type="text" id="first-name" />
 
             <label htmlFor="last-name">Last Name</label>
-            <input
-                type="text"
-                id="last-name"
-                onChange={(e) =>
-                    setEmployee({ ...employee, lastname: e.target.value })
-                }
-            />
+            <input name="last-name" type="text" id="last-name" />
 
             <label htmlFor="date-of-birth">Date of Birth</label>
-            <input
-                id="date-of-birth"
-                type="text"
-                onChange={(e) =>
-                    setEmployee({ ...employee, birthdate: e.target.value })
-                }
-            />
+            <input name="date-of-birth" id="date-of-birth" type="date" />
 
             <label htmlFor="start-date">Start Date</label>
-            <input
-                id="start-date"
-                type="text"
-                onChange={(e) =>
-                    setEmployee({ ...employee, startdate: e.target.value })
-                }
-            />
+            <input name="start-date" id="start-date" type="date" />
 
             <fieldset className="address">
                 <legend>Address</legend>
 
                 <label htmlFor="street">Street</label>
-                <input
-                    id="street"
-                    type="text"
-                    onChange={(e) =>
-                        setEmployee({ ...employee, street: e.target.value })
-                    }
-                />
+                <input name="street" id="street" type="text" />
 
                 <label htmlFor="city">City</label>
-                <input
-                    id="city"
-                    type="text"
-                    onChange={(e) =>
-                        setEmployee({ ...employee, city: e.target.value })
-                    }
-                />
+                <input name="city" id="city" type="text" />
 
                 <label htmlFor="state">State</label>
-                <select
+                <DropDownMenu
                     name="state"
                     id="state"
-                    onChange={(e) =>
-                        setEmployee({ ...employee, state: e.target.value })
-                    }
-                ></select>
+                    className=""
+                    data={states}
+                />
 
                 <label htmlFor="zip-code">Zip Code</label>
-                <input
-                    id="zip-code"
-                    type="number"
-                    onChange={(e) =>
-                        setEmployee({
-                            ...employee,
-                            zipcode: e.target.value
-                        })
-                    }
-                />
+                <input name="zip-code" id="zip-code" type="number" />
             </fieldset>
 
             <label htmlFor="department">Department</label>
-            <select
+            <DropDownMenu
                 name="department"
                 id="department"
-                onChange={(e) =>
-                    setEmployee({ ...employee, department: e.target.value })
-                }
-            >
-                <option>Sales</option>
-                <option>Marketing</option>
-                <option>Engineering</option>
-                <option>Human Resources</option>
-                <option>Legal</option>
-            </select>
+                className=""
+                data={departments}
+            />
             <button>Save</button>
         </form>
     );
