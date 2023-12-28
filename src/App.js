fc4c1d93352f6e5e87ebd87fdab53a82da5986a4
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-12-28 17:07:26 +0100

    Removed useless lines

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import './style/sass/style.scss';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import EmployeeList from './pages/employee-list/EmployeeList';
-import { fetchEmployeeData } from './store/EmployeeSlice';
-import { useDispatch } from 'react-redux';
 
 function App() {
-
-  // const dispatch = useDispatch();
-  // dispatch(fetchEmployeeData());
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/employee-list" element={<EmployeeList />}/>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/employee-list" element={<EmployeeList />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
