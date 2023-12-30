d77013b7cc631cd9e54c13bc5c0965e7de6be717
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-12-30 02:54:52 +0100

    Removed unused imports

--- src/components/table/TableHeader.js
+++ src/components/table/TableHeader.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React from 'react';
 import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
 
 export default function TableHeader({ columns, sortField, setSortField, sortOrder, setSortOrder }) {
